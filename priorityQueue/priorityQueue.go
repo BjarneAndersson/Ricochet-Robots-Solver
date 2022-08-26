fd7b41d67882054256195339523d0e9704b438c7
Bjarne Andersson <bjarne4andersson@gmail.com>
2022-08-26 21:42:53 +0200

    :sparkles: Base component for the solver

---
+++ priorityQueue/priorityQueue.go
@@ -0,0 +1,52 @@
+package priorityQueue
+
+import (
+	"../types"
+	"container/heap"
+)
+
+// An Item is something we manage in a priority queue.
+type Item struct {
+	Value    types.BoardState
+	Priority int
+	Index    int
+}
+
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the lowest, not highest, priority, so we use less than here.
+	return pq[i].Priority < pq[j].Priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
+
+func (pq *PriorityQueue) Push(x any) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.Index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() any {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil  // avoid memory leak
+	item.Index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the priority and value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, value types.BoardState, priority int) {
+	item.Value = value
+	item.Priority = priority
+	heap.Fix(pq, item.Index)
+}
