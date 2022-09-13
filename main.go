f13f505bc2d574b87090e520d91d23e46f7b900a
Bjarne Andersson <bjarne4andersson@gmail.com>
2022-09-13 17:09:09 +0200

    :zap: Add error handling

--- main.go
+++ main.go
@@ -10,12 +10,14 @@ func main() {
 	board, initBoardState, err := input.GetData()
 	if err != nil {
 		fmt.Printf("Error loading board data:\n%v\n", err)
+		return
 	}
 
 	fmt.Printf("%+v\n", board)
 
 	path, err := solver.Solver(&board, initBoardState)
 	if err != nil {
+		fmt.Printf("Error solving:\n%v\n", err)
 		return
 	}
 
