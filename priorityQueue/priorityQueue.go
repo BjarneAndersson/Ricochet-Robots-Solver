c206c432738f40aa7d4ae65e11c91f423f386f64
Bjarne Andersson <bjarne4andersson@gmail.com>
2022-09-13 20:53:07 +0200

    :bug: Fix and simplify priority queue for my needs

--- priorityQueue/priorityQueue.go
+++ priorityQueue/priorityQueue.go
@@ -2,51 +2,33 @@ package priorityQueue
 
 import (
 	"../types"
-	"container/heap"
+	"sort"
 )
 
 // An Item is something we manage in a priority queue.
 type Item struct {
 	Value    types.BoardState
 	Priority int
-	Index    int
 }
 
-type PriorityQueue []*Item
+type PriorityQueue []Item
 
-func (pq PriorityQueue) Len() int { return len(pq) }
+func (pq *PriorityQueue) Len() int { return len(*pq) }
 
-func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the lowest, not highest, priority, so we use less than here.
-	return pq[i].Priority < pq[j].Priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].Index = i
-	pq[j].Index = j
-}
-
-func (pq *PriorityQueue) Push(x any) {
-	n := len(*pq)
-	item := x.(*Item)
-	item.Index = n
+func (pq *PriorityQueue) Push(item Item) {
 	*pq = append(*pq, item)
+	sortQueue(*pq)
 }
 
-func (pq *PriorityQueue) Pop() any {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // avoid memory leak
-	item.Index = -1 // for safety
-	*pq = old[0 : n-1]
+func sortQueue(pq PriorityQueue) {
+	// We want Pop to give us the lowest, not highest, priority, so we use less than here.
+	sort.Slice(pq, func(i, j int) bool {
+		return (pq)[i].Priority < (pq)[j].Priority
+	})
+}
+
+func Pop(pq *PriorityQueue) Item {
+	item := (*pq)[0]
+	*pq = append((*pq)[:0], (*pq)[1:]...)
 	return item
-}
-
-// update modifies the priority and value of an Item in the queue.
-func (pq *PriorityQueue) update(item *Item, value types.BoardState, priority int) {
-	item.Value = value
-	item.Priority = priority
-	heap.Fix(pq, item.Index)
 }
