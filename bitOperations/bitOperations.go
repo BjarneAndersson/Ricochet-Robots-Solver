99c5aae628d6cf7f2dbe7d3509b193d6faf46970
Bjarne Andersson <bjarne4andersson@gmail.com>
2022-09-16 16:21:56 +0200

    :ambulance: Implement clearBit functionality

--- bitOperations/bitOperations.go
+++ bitOperations/bitOperations.go
@@ -2,9 +2,19 @@ package bitOperations
 
 // SetBit Sets the bit at pos in the byte pB.
 func SetBit(pB *byte, position uint8, value bool) {
-	n := int(*pB)
-	n |= boolToInteger(value) << position
-	*pB = byte(n)
+	if value {
+		n := int(*pB)
+		n |= boolToInteger(value) << position
+		*pB = byte(n)
+	} else {
+		clearBit(pB, position)
+	}
+}
+
+// Clears the bit at pos in n.
+func clearBit(pB *uint8, position uint8) {
+	mask := ^(1 << position)
+	*pB &= uint8(mask)
 }
 
 // HasBit Checks whether the bit at pos n is set.
