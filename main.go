08dbf388f44b5cf9b1c4503bc22d58093459b675
Bjarne Andersson <bjarne4andersson@gmail.com>
2022-09-17 12:11:09 +0200

    :construction: Implement computation of robot stopping positions

--- main.go
+++ main.go
@@ -2,24 +2,30 @@ package main
 
 import (
 	"./input"
+	"./precomputation"
 	"./solver"
 	"fmt"
 )
 
 func main() {
+	// convert data into board object
 	board, initBoardState, err := input.GetData()
 	if err != nil {
 		fmt.Printf("Error loading board data:\n%v\n", err)
 		return
 	}
-
 	fmt.Printf("%+v\n", board)
+
+	// calculate stopping positions for each node
+	robotMoves, err := precomputation.PrecomputeRobotMoves(&board)
+	fmt.Printf("\n%+v\n", robotMoves)
+
+	// solve the board
 
 	path, err := solver.Solver(&board, initBoardState)
 	if err != nil {
-		fmt.Printf("Error solving:\n%v\n", err)
+		fmt.Printf("\nError solving:\n%v\n", err)
 		return
 	}
-
-	fmt.Printf("%+v\n", path)
+	fmt.Printf("\n%+v\n", path)
 }
--- precomputation/precomputation.go
+++ precomputation/precomputation.go
@@ -53,3 +53,40 @@ func PrecomputeBoard(board *types.Board) (err error) {
 func setMoveCount(pB *byte, moveCount uint8) {
 	*pB = (31 & *pB) | (moveCount << 5)
 }
+
+func PrecomputeRobotMoves(board *types.Board) (robotMoves map[types.Position]map[string]types.Position, err error) {
+	robotMoves = make(map[types.Position]map[string]types.Position)
+
+	for rowIndex := 0; rowIndex < 16; rowIndex++ {
+		for columnIndex := 0; columnIndex < 16; columnIndex++ {
+			robotMoves[types.Position{Row: uint8(rowIndex), Column: uint8(columnIndex)}] = map[string]types.Position{"left": {}, "right": {}, "top": {}, "bottom": {}}
+			for _, direction := range []string{"left", "right", "top", "bottom"} {
+				robotMoves[types.Position{Row: uint8(rowIndex), Column: uint8(columnIndex)}][direction] = calculateRobotStoppingPosition(board, types.Position{Row: uint8(rowIndex), Column: uint8(columnIndex)}, direction)
+			}
+		}
+	}
+
+	return robotMoves, nil
+}
+
+func calculateRobotStoppingPosition(board *types.Board, startNodePosition types.Position, direction string) types.Position {
+	cNodePosition := startNodePosition
+	cNode := board.Board[cNodePosition.Row][cNodePosition.Column]
+
+	for helper.HasNeighbor(cNode, direction) {
+		cNode = board.Board[cNodePosition.Row][cNodePosition.Column]
+
+		switch direction {
+		case "left":
+			cNodePosition = types.Position{Column: cNodePosition.Column - 1, Row: cNodePosition.Row}
+		case "right":
+			cNodePosition = types.Position{Column: cNodePosition.Column + 1, Row: cNodePosition.Row}
+		case "top":
+			cNodePosition = types.Position{Column: cNodePosition.Column, Row: cNodePosition.Row - 1}
+		case "bottom":
+			cNodePosition = types.Position{Column: cNodePosition.Column, Row: cNodePosition.Row + 1}
+		}
+	}
+
+	return cNodePosition
+}
