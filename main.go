2bfbc0fd9d566223cf9f858bff7a99eebef1a007
Bjarne Andersson <bjarne4andersson@gmail.com>
2022-09-20 16:32:22 +0200

    :sparkles: Add logging

--- main.go
+++ main.go
@@ -4,28 +4,28 @@ import (
 	"./input"
 	"./precomputation"
 	"./solver"
-	"fmt"
+	"log"
 )
 
 func main() {
 	// convert data into board object
 	board, initBoardState, err := input.GetData()
 	if err != nil {
-		fmt.Printf("Error loading board data:\n%v\n", err)
+		log.Printf("Error loading board data:\n%v\n", err)
 		return
 	}
-	fmt.Printf("%+v\n", board)
+	log.Printf("%+v\n", board)
 
 	// calculate stopping positions for each node
 	robotMoves, err := precomputation.PrecomputeRobotMoves(&board)
-	fmt.Printf("\n%+v\n", robotMoves)
+	log.Printf("\n%+v\n", robotMoves)
 
 	// solve the board
 
 	path, err := solver.Solver(&board, initBoardState)
 	if err != nil {
-		fmt.Printf("\nError solving:\n%v\n", err)
+		log.Printf("\nError solving:\n%v\n", err)
 		return
 	}
-	fmt.Printf("\n%+v\n", path)
+	log.Printf("%+v\n", path)
 }
