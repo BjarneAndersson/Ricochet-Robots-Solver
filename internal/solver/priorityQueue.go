ddeb4ee8234aafd8507e31d4e7a3856765978924
Bjarne Andersson <bjarne4andersson@gmail.com>
2022-10-20 20:04:38 +0200

    :bug: Fix queue sorting

--- internal/solver/priorityQueue.go
+++ internal/solver/priorityQueue.go
@@ -22,7 +22,7 @@ func (pq *priorityQueue) Push(item item) {
 
 func (pq *priorityQueue) sortQueue() {
 	// We want Pop to give us the lowest, not highest, priority, so we use less than here.
-	sort.Slice(pq, func(i, j int) bool {
+	sort.Slice(*pq, func(i, j int) bool {
 		return calcPriority((*pq)[i].HAndGScore) < calcPriority((*pq)[j].HAndGScore)
 	})
 }
